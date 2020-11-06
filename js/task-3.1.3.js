8f70186f59cd292aff11ecf74d2f9c7aaaf3adfc
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-06 21:03:41 +0000

    Update task-3.1.3.js

--- js/task-3.1.3.js
+++ js/task-3.1.3.js
@@ -5,3 +5,15 @@ console.log(numberOfLicenses);
 alert("number of licenses = " + numberOfLicenses);
 
 document.write("Number of licenses = " + numberOfLicenses);
+
+/* 
+There are various different datatypes:- 
+1.string 
+2.number - can be floats (decimal point) and integer). JS changes integers to floats. NaN = Not a Number (this is an error you may see in console)
+3.boolean 
+4.undefinded 
+5.null 
+6.symbol 
+7.Object 
+
+*/
