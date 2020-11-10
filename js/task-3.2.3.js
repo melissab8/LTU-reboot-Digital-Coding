22f966731e65b9eede514998de6b56b61c331500
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-10 20:19:35 +0000

    updated task 3.2.3
    using variables and functions

---
+++ js/task-3.2.3.js
@@ -0,0 +1,16 @@
+function fullName (firstName, lastname){
+return firstName + " " + lastname ;
+}
+
+//I have declared the variable here and the return function will return the values later
+
+var userName = fullName("Melissa", "Brown");
+//here is have declared a variable
+
+console.log(userName); 
+//here is am console logging out the result
+
+
+
+
+
