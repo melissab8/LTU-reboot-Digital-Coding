0e1addf06073bc2b84e2461becbd4fd0e3ca6019
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-10 21:07:28 +0000

    updated task 3.2.5.js
    added if and else statements as part of control flow

---
+++ js/task-3.2.5.js
@@ -0,0 +1,29 @@
+var temperature = -5;
+//as i change the temp in the var the output in the console log will change
+
+if (temperature < 0) {
+    console.log("stay inside");
+
+} else if (temperature < 30){
+console.log("wear coat and hat");
+
+
+} else if (temperature < 50) {
+    console.log("wear a coat");
+} else {
+    console.log("wear pants and vest");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+  
