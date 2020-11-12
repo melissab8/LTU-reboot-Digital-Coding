fd30c62573a02fbc5301700c3cce2e1124857c02
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-12 21:06:04 +0000

    updated task 3.3.3.js
    created a function with operators + - / * and %.
    
    Added text to 3.3.3 html

---
+++ js/task-3.3.3.js
@@ -0,0 +1,38 @@
+function calculator( number1, number2, operator) {
+ 
+
+    var result = " ";
+    //this var will output my result
+
+
+switch( operator ){
+     case " + " :
+        result = number1 + number2;
+        console.log(number1 + " + " + number2 + " = " + result);
+        break;
+
+    case " - " :
+        result = number1 - number2;
+        console.log(number1 + " - " + number2 + " = " + result);
+        break;
+
+    case " / " :
+        //code here
+        break;
+
+    case " % " :
+        //code here
+        break;
+
+     default :  
+     //write an error message   
+
+ }
+
+
+
+
+
+}
+
+// + ,-, % , *, / these are the operators i need to use. In browser console i need to write function(5,5,"+"); to see the output. 
