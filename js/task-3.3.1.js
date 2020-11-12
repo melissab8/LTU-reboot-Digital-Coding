cbb22ee5b270ac109dd0841e11a333bf1aabd9ec
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-12 19:49:02 +0000

    Update task-3.3.1.js
    Created a calculator in task 3.3.1.js using functions

---
+++ js/task-3.3.1.js
@@ -0,0 +1,16 @@
+
+function percentageCalculator(number, percentage) {
+    var percentageOf = number / 100 * percentage;
+    
+    return percentageOf;
+
+}
+
+console.log (percentageCalculator (300 , 10));
+
+
+//You can write this directly in the console and you can change the values as you want.
+
+
+
+//BODMAS you can do this in a different order to what i have done to get a % var could be (number * percentage) /100; my way above is much easier!
