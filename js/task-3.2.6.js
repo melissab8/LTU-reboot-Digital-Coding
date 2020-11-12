d7b6f19c306f228f052ba4a20f135d71b75ce4a2
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-12 19:27:45 +0000

    Updated task 3.2.6.js
    added a logical operator to the task 3.2.6.js

---
+++ js/task-3.2.6.js
@@ -0,0 +1,21 @@
+var temperature = 54;
+
+
+if (temperature < 0) {
+    console.log("stay inside");
+} else if (temperature < 30){
+console.log("wear coat and hat");
+} else if (temperature < 50) {
+    console.log("wear a coat");
+} else if(temperature > 50 && temperature ===55){
+    console.log("the and operator - buy a fan!");
+} else if (temperature > 50 || temperature === 55){
+    console.log("the or operator - it's very hot");
+} else {
+    console.log("wear pants and vest");
+
+
+}
+
+
+//adding logical operators && (which is and) and || (which is or) ! (which is not)
