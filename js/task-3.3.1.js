4ddf125737b22f75c6694520aff554948c758f75
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-15 16:41:50 +0000

    Update task-3.3.1.js

--- js/task-3.3.1.js
+++ js/task-3.3.1.js
@@ -1,16 +1,14 @@
 
 function percentageCalculator(number, percentage) {
-    var percentageOf = number / 100 * percentage;
+    var percentageOf = (number * percentage) /100; 
     
     return percentageOf;
 
 }
 
 console.log (percentageCalculator (300 , 10));
-
-
 //You can write this directly in the browser console and you can change the values as you want.
 
 
 
-//BODMAS you can do this in a different order to what i have done to get a %.    var could be (number * percentage) /100; my way above is much easier!
+//BODMAS you should alway do the sum in the brackets first. In coding and maths alike the general rule is do the multiplication or division first, THEN do the addition or subtraction. To make it clearer we add brackets. (or you could just do e.g 85/100*15= 12.75   to get 15% of £85.00)
