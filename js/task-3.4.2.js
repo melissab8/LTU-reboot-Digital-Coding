68d828c7c91b841ab0b124a05ba002b80901adce
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-17 20:17:35 +0000

    Update task-3.4.2.js
    added an array and for loop to display a list of food

---
+++ js/task-3.4.2.js
@@ -0,0 +1,13 @@
+//this is creating a variable with an array and then using a for loop to output this in the console.log
+
+
+var favFoods = ["pizza", "Cheese", "chocolate","chips"];
+
+for( var index = 0; index < favFoods.length; index++ ) {
+   console.log( favFoods[index] );
+
+
+}
+
+
+
