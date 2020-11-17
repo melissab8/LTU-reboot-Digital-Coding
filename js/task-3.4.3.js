249d32d378194738e06fcb717012a4cb53290ef9
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-17 20:32:15 +0000

    Update task-3.4.3.js

--- js/task-3.4.3.js
+++ js/task-3.4.3.js
@@ -1,7 +1,10 @@
-var favFoods = ["pizza", "Cheese", "chocolate","chips"];
+var favFoods = ["pizza", "cheese", "chocolate","chips"];
 
 for( var index = 0; index < favFoods.length; index++ ) {
-   document.write( favFoods[index] + "<br>" );
+    console.log( favFoods[index]);
+    document.write( favFoods[1] + "<br>" );
 
 
 }
+
+//the index always starts with 0, pizza is 0, cheese is 1, choc is 3 chips is 4
