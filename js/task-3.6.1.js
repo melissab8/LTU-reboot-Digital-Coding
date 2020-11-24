947a000f5ae34ba3d54cdaf1eef446e662c65bb5
Mel Brown <32229718+melissab8@users.noreply.github.com>
2020-11-24 20:17:54 +0000

    Updated JS for task-3.6.1.js

---
+++ js/task-3.6.1.js
@@ -0,0 +1,13 @@
+/*var pageBody = document.querySelector("body")[0]
+
+pageBody.style.backgroundColor = "red";
+pageBody.style.paddingTop = "10px";
+*/
+
+//this is changing the paragraph color to blue
+var paragraph = document.querySelector("p");
+ paragraph.style.color = "blue";
+
+ //this is changing the image
+ var image1 = document.querySelector( "img"); 
+ image1.setAttribute("src", "../images/w3quiz.png");
